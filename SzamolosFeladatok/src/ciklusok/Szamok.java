d0b81c783f8ad5b575a4ebf517d5aa265235d16f
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-03-02 13:53:09 +0100

    Nem lett teljesen kesz

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,19 +1,24 @@
-
 package ciklusok;
 
+import java.util.Scanner;
 
 public class Szamok {
-        public static void main(String[] args) {
-        
-            
 
-            for (int j = 2; j <= 4; j++) {
-                for (int i = 2; i <= 4; i++){
-                    for (int k = 2; k <= 4; k++){
-                    System.out.printf("%d%d%d\n",j,i,k);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Szeretné hogy legyen szám ismétlés ? \n Igen | Nem\n");
+        String valasz = sc.nextLine();
+        boolean IgazHamis = valasz.equals("igen");
+
+        for (int j = 2; j <= 4; j++) {
+            for (int i = 2; i <= 4; i++) {
+                for (int k = 2; k <= 4; k++) {
+                    if (!IgazHamis && i != k && i != j && j != k) {
+                        System.out.printf("%d%d%d\n", j, i, k);
                     }
+                    System.out.printf("%d%d%d\n", j, i, k);
                 }
             }
+        }
     }
-    
 }
